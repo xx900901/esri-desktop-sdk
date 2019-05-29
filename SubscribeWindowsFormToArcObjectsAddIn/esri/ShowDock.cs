76f1bebed6900b31416f2a8037a9835510ed0898
xx900901 <51140917+xx900901@users.noreply.github.com>
2019-05-29 10:53:56 -0700

    How to subscribe windows form to ArcGIS Addin Tools
    This sample codes demonstrates how to activate the selection tool from a button in windows form

---
+++ SubscribeWindowsFormToArcObjectsAddIn/esri/ShowDock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Framework;
+
+namespace esri
+{
+    public class ShowDock : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public ShowDock()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            UID dockableWinUID = new UIDClass();
+            dockableWinUID.Value = ThisAddIn.IDs.DockableWindow1;
+            IDockableWindow DockableWin = ArcMap.DockableWindowManager.GetDockableWindow(dockableWinUID);
+            DockableWin.Show(true);
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
